9530b92312ef1b70e875865e20ebfc44fd6fd075
Rada2603 <145929352+Rada2603@users.noreply.github.com>
2023-11-03 17:29:59 +0100

    povezivanje

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("hello")
+import mysql.connector
+from mysql.connector import Error
+
+db_config = {
+    "host": "35.239.158.202",
+    "user": "root",
+    "password": "7$x[;*X-0qpT[J2n",
+    "database": "test",
+}
+
+try:
+    connection = mysql.connector.connect(**db_config)
+    if connection.is_connected():
+        print("Uspješno povezan na bazu podataka")
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM test.store limit 2")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+
+except Error as e:
+    print("Greška prilikom povezivanja na bazu podataka:", e)
+
+
+finally:
+    # Zatvori vezu
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("Veza zatvorena")
