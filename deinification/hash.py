6681687957d9ab19da81b30093f32104ac8a9ce0
Rada2603 <145929352+Rada2603@users.noreply.github.com>
2023-11-11 21:40:27 +0100

    finish

--- deinification/hash.py
+++ deinification/hash.py
@@ -21,20 +21,23 @@ def deidentify_passwords_and_save_to_json():
     cursor.execute(sql_queries["select_saler_passwords"])
     results = cursor.fetchall()
 
-    deidentified_passwords = {}
+    with open(r"deidentified_passwords.json") as f:
+        file = json.load(f)
     for result in results:
         original_password = result[0]
-        if original_password not in deidentified_passwords:
+        print(original_password)
+        if original_password not in file.values():
+            print(file.values())
             deidentified_password = hash_password(original_password)
-            deidentified_passwords[original_password] = deidentified_password
+            file[original_password] = deidentified_password
+            with open("deidentified_passwords.json", "w") as f:
+                json.dump(file, f, indent=3)
             cursor.execute(
-                sql_queries["select_hash"], (deidentified_password, original_password)
+                sql_queries["select_hash"],
+                (deidentified_password, original_password),
             )
         else:
             continue
-
-    with open("deidentified_passwords.json", "w") as json_file:
-        json.dump(deidentified_passwords, json_file, indent=3)
 
     connection.commit()
     close_conection(connection, cursor)
